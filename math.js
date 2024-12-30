bca01c65b0cb4edee1dc85ce346d97091f30df72
sandhya-penumaka <151710125+sandhya-penumaka@users.noreply.github.com>
2024-12-30 16:16:06 +0530

    Create math.js

---
+++ math.js
@@ -0,0 +1,12 @@
+const addition=(a,b)=>a+b
+const subtraction=(a,b)=>a-b;
+const multiplication=(a,b)=>a*b;
+const division=(a,b)=>a/b;
+const remainder=(a,b)=>a%b;
+module.exports={addition,subtraction,multiplication,division,remainder};
+
+exports.addition=(a,b)=>a+b;
+exports.subtraction=(a,b)=>a-b;
+exports.multiplication=(a,b)=>a*b;
+exports.division=(a,b)=>a/b;
+exports.remainder=(a,b)=>a%b;
